0e161909e4694da7cf09b20304dd9c091921cb50
dank6969 <83874118+dank6969@users.noreply.github.com>
2021-09-10 17:52:41 -0400

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,4 +1,4 @@
-if game.PlaceVersion ~= 5176 then -- REPLACE 727 WITH THE NUMBER IN DEVCONSOLE 
+if game.PlaceVersion ~= 5221 then -- REPLACE 727 WITH THE NUMBER IN DEVCONSOLE 
 	game.Players.LocalPlayer:Kick("\nGUI IS OUTDATED, WAIT FOR UPDATE\nJOIN DISCORD FOR MORE INFO https://discord.gg/leafy") -- put whatever message you want here 
 	wait(0.2)
 	while true do end
