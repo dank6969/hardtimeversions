d8b49124ba2f4291b1cdb3f316764370b48badf5
dank6969 <83874118+dank6969@users.noreply.github.com>
2021-09-12 17:38:13 -0400

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,13 +1,1 @@
-if game.PlaceVersion ~= 5228 then -- REPLACE 727 WITH THE NUMBER IN DEVCONSOLE 
-	warn(game.PlaceVersion)
-	game.Players.LocalPlayer:Kick("\nGUI IS OUTDATED, WAIT FOR UPDATE\nJOIN DISCORD FOR MORE INFO https://discord.gg/leafy") -- put whatever message you want here 
-	wait(0.2)
-	while true do end
-	else
-	game.StarterGui:SetCore("SendNotification", {
-	Title = "Dank-Ware";
-	Text = "Dank-Ware Is up to date!"; -- put whatever message you want here 
-	Icon = "";
-	Duration = 5;
-	})
-	end
+warn'lol'
